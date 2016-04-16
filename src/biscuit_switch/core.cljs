db3d16c963ab90f077ee42bc3228f7f1dd488bcd
Crispin Wellington <retrogradeorbit@gmail.com>
2016-04-16 21:32:08 +0800

    load in and create fonts

--- src/biscuit_switch/core.cljs
+++ src/biscuit_switch/core.cljs
@@ -2,18 +2,23 @@
   (:require [biscuit-switch.assets :as assets]
             [biscuit-switch.game :as game]
             [biscuit-switch.player :as player]
+            [biscuit-switch.text :as text]
+            [biscuit-switch.stamper :as stamper]
 
             [infinitelives.pixi.canvas :as c]
             [infinitelives.pixi.events :as e]
             [infinitelives.pixi.resources :as r]
             [infinitelives.pixi.texture :as t]
             [infinitelives.pixi.sprite :as s]
+            [infinitelives.pixi.pixelfont :as pf]
             [infinitelives.utils.math :as math]
             [infinitelives.utils.sound :as sound]
 
             [cljs.core.async :refer [<!]])
   (:require-macros [cljs.core.async.macros :refer [go]]
-                   [infinitelives.pixi.macros :as m]))
+                   [infinitelives.pixi.macros :as m]
+                   [infinitelives.pixi.pixelfont :as pf]
+                   ))
 
 (defonce canvas
   (c/init {:layers [:bg :belt :machines :player :tables :ui]
@@ -25,7 +30,15 @@
   (go
     (<! (r/load-resources canvas :ui
                           ["img/sprites.png"
+                           "img/fonts.png"
                            "sfx/bloop.ogg"]))
+
+    (pf/pixel-font :font "img/fonts.png" [12 118] [229 167]
+                   :chars ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                           "abcdefghijklmnopqrstuvwxyz"
+                           "0123456789!?#`'.,"]
+                   :kerning {"fo" -2  "ro" -1 "la" -1 }
+                   :space 5)
 
     (sound/play-sound :bloop 1.00 false)
 
@@ -38,7 +51,7 @@
        (map
         #(s/make-sprite :conveyor :scale 4
                         :x (* 64 %)
-                       ;; TODO: get tiling going in infinitelives
+                        ;; TODO: get tiling going in infinitelives
                                         ;:tiling :tiling-width 100 :tiling-height 100
                         )
         (range -10 10)
@@ -46,7 +59,6 @@
       (m/with-sprite canvas :machines
         [
          roller (s/make-sprite :roller :scale 4 :x -350 :y -72)
-         stamper (s/make-sprite :stamper :scale 4 :y -74)
          oven (s/make-sprite :oven :scale 4 :x 450 :y -148)
          tv (s/make-sprite :tv :scale 4 :x 300 :y -280)
          tri-table (s/make-sprite :tri-table :scale 4 :x -250 :y 275)
@@ -54,13 +66,19 @@
          square-table (s/make-sprite :square-table :scale 4 :x 250 :y 300)
          ]
 
+        ;; "threads"
         (player/player canvas)
+        (text/text-thread canvas)
+        (stamper/stamper-thread canvas)
 
         (loop [c 20000]
 
             (<! (e/next-frame))
             (recur (dec c))
-            ))
+            )
+
+)
 
       )
-))
+
+    ))
