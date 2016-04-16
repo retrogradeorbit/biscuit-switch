83b0713267de3c28a88193a7d54aa11fdfc2aa70
Crispin Wellington <retrogradeorbit@gmail.com>
2016-04-16 12:28:58 +0800

    setup more assets

--- src/biscuit_switch/assets.cljs
+++ src/biscuit_switch/assets.cljs
@@ -1,6 +1,47 @@
 (ns biscuit-switch.assets)
 
 (def assets
-  {:conveyer
+  {:conveyor
    {:pos [160 85]
-    :size [16 28]}})
+    :size [16 28]}
+
+   :stamper
+   {:pos [96 48]
+    :size [48 65]}
+
+   :roller
+   {:pos [16 48]
+    :size [67 65]}
+
+   :tv
+   {:pos [150 38]
+    :size [64 51]}
+
+   :oven
+   {:pos [281 7]
+    :size [87 110]}
+
+   :player-1
+   {:pos [224 80]
+    :size [32 32]}
+
+   :player-2
+   {:pos [256 80]
+    :size [32 32]}
+
+   :player-3
+   {:pos [288 80]
+    :size [32 32]}
+
+   :tri-table
+   {:pos [43 132]
+    :size [53 42]}
+
+   :square-table
+   {:pos [43 132]
+    :size [53 42]}
+
+   :circle-table
+   {:pos [43 132]
+    :size [53 42]}
+})
